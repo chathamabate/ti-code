4b1d77e0b597f45751eeb9a36ce080579d8dd09f
chathamabate <chathamabate@gmail.com>
2022-05-18 11:14:37 -0400

    Gameloop template.

--- snake/src/main.c
+++ snake/src/main.c
@@ -13,12 +13,15 @@
 #include "scores.h"
 #include "snake_render.h"
 
+#define DELAY 50
+
 #define START_PAGE      0
 #define HIGHSCORES      1
 #define IN_PLAY         2
 #define DEFEAT          3
 #define NEW_HIGHSCORE   4
-#define EXIT            5
+#define EXIT            5 
+// For sizing, make sure exit is always the last state.
 
 #define SB_ERROR() \
     os_ClrHome(); \
@@ -26,7 +29,14 @@
     while (os_GetCSC() != sk_Clear); \
     return 0
 
+// Simple typedef for runnable actions.
+typedef  void (*action)(void);
+
+// The state of the game.
 static uint8_t curr_state = START_PAGE;
+
+// Whether or not a redraw is needed.
+static uint8_t redraw_needed = 1;
 
 static button main_menu_bts[3];
 static menu *main_menu;
@@ -34,14 +44,44 @@ static menu *main_menu;
 static button defeat_menu_bts[2];
 static menu *defeat_menu;
 
-static menu *selected_menu = NULL;
+static button highscores_menu_bts[1];
+static menu *highscores_menu;
+
+// Menus for each state.
+static menu *menus[EXIT + 1];
+
 static hs_entry *sb = NULL;
+
+// Whether or not a new highscore has been achieved.
+static uint8_t archive_needed = 0;
+
+// These are the render and update functions for each game
+// state.
+static action render_funcs[EXIT + 1];
+static action update_funcs[EXIT + 1];
 
 // Initialize above globals.
 void init_globals(void);
 
 // Clean up above globals.
 void cleanup_globals(void);
+
+// All scene render functions.
+void render_start_page(void);
+void render_highscores(void);
+void render_in_play(void);
+void render_defeat(void);
+void render_new_highscore(void);
+void render_exit(void); // Should never be called.
+
+// This update function is usable for
+// START_PAGE, HIGH_SCORES, and DEFEAT
+
+// All update functions.
+void update_menus(void);
+void update_in_play(void);
+void update_new_highscore(void);
+void update_exit(void);
 
 void init_globals(void) {
     main_menu_bts[0].text = "Play";   
@@ -62,6 +102,40 @@ void init_globals(void) {
     defeat_menu_bts[1].link = START_PAGE;
 
     defeat_menu = new_menu(defeat_menu_bts, 2);
+
+    highscores_menu_bts[0].text = "Main Menu";
+    highscores_menu_bts[0].link = START_PAGE;
+
+    highscores_menu = new_menu(highscores_menu_bts, 1);
+
+    menus[START_PAGE] = main_menu;
+    menus[HIGHSCORES] = highscores_menu;
+    menus[IN_PLAY] = NULL;
+    menus[DEFEAT] = defeat_menu;
+    menus[NEW_HIGHSCORE] = NULL;
+    menus[EXIT] = NULL;
+
+    render_funcs[START_PAGE] = render_start_page;
+    render_funcs[HIGHSCORES] = render_highscores;
+    render_funcs[IN_PLAY] = render_in_play;
+    render_funcs[DEFEAT] = render_defeat;
+    render_funcs[NEW_HIGHSCORE] = render_new_highscore;
+    render_funcs[EXIT] = render_exit;
+
+    render_funcs[START_PAGE] = update_menus;
+    render_funcs[HIGHSCORES] = update_menus;
+    render_funcs[IN_PLAY] = update_in_play;
+    render_funcs[DEFEAT] = update_menus;
+    render_funcs[NEW_HIGHSCORE] = render_new_highscore;
+    render_funcs[EXIT] = update_exit;
+}
+
+void cleanup_globals(void) {
+    // Free all dynamic memory globals.
+    free(main_menu);
+    free(defeat_menu);
+    free(highscores_menu);
+    free(sb);
 }
 
 int main(void) {
@@ -76,39 +150,28 @@ int main(void) {
     gfx_SetDrawBuffer();
     gfx_Begin();
 
+    // Simple game loop.
     while (curr_state != EXIT) {
-        gfx_FillScreen(255);
-        sk_key_t key = os_GetCSC();
+        if (redraw_needed) {
+            gfx_FillScreen(255);
+            (render_funcs[curr_state])();
+            gfx_SwapDraw();
 
-        if (curr_state == IN_PLAY) {
-
-            continue;
+            redraw_needed = 0;
         }
 
-        if (curr_state == NEW_HIGHSCORE) {
-
-            continue;
-        }
-
-        // Default case...
-        // just normal menu manipulation.
-
-        if (key == sk_Up) {
-            
-        }
-
+        (update_funcs[curr_state])();
+        delay(DELAY);
     }
 
-    gfx_FillScreen(255);
-
-    render_sb(sb, c);
-
-    gfx_SwapDraw();
-    gfx_Wait();
-
-    while (os_GetCSC() != sk_Clear);
-
     gfx_End();
-    free(sb);
+
+    if (archive_needed && !archive_sb(sb)) {
+        cleanup_globals();
+        SB_ERROR(); // archive error!
+    }
+
+    cleanup_globals();
+
     return 0;
 }
