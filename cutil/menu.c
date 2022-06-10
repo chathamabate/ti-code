2273b7933d4c70c23e3317879e1a6c129925fa60
chathamabate <chathamabate@gmail.com>
2022-06-09 22:57:35 -0400

    BED TIME!

--- cutil/menu.c
+++ cutil/menu.c
@@ -8,7 +8,8 @@
 #include <cutil/misc.h>
 #include <cutil/cgraphx.h>
 
-void render_text_menu_button_nc(text_menu *menu, uint16_t x, uint8_t y, uint8_t i) {
+// Render a single button of a menu.
+void render_text_menu_button_nc(text_menu *menu, uint8_t i) {
     const text_menu_template *template = menu->template;
     const cgfx_pane_style *pane_style = template->style_palette[menu->styles[i]];
 
@@ -16,11 +17,11 @@ void render_text_menu_button_nc(text_menu *menu, uint16_t x, uint8_t y, uint8_t 
     uint8_t y_p; 
 
     if (template->format == MENU_VERTICAL) {
-        x_p = x;
-        y_p = y + (i * template->button_height);
+        x_p = template->x;
+        y_p = template->y + (i * template->button_height);
     } else {
-        x_p = x + (i * template->button_width);
-        y_p = y;
+        x_p = template->x + (i * template->button_width);
+        y_p = template->y;
     }
 
     cgfx_pane_nc(pane_style, x_p, y_p, template->button_width, template->button_height);
@@ -37,62 +38,111 @@ void render_text_menu_button_nc(text_menu *menu, uint16_t x, uint8_t y, uint8_t 
     );
 }
 
-void render_text_menu_nc(text_menu *menu, uint16_t x, uint8_t y) {
+void render_text_menu_nc(text_menu *menu) {
     uint8_t i;
     for (i = 0; i < menu->template->len; i++) {
-        render_text_menu_button_nc(menu, x, y, i);
+        render_text_menu_button_nc(menu, i);
     }
 }
 
-basic_text_menu *new_basic_text_menu(const text_menu_template *tmplt, uint8_t s_style, uint8_t ds_style) {
-    basic_text_menu *bt_menu = safe_malloc(sizeof(basic_text_menu));
+simple_text_menu *new_simple_text_menu(const text_menu_template *tmplt, 
+    uint8_t s_style, uint8_t ds_style, uint8_t uf_style) {
+    
+    simple_text_menu *st_menu = safe_malloc(sizeof(simple_text_menu));
 
-    bt_menu->menu.template = tmplt;
-    bt_menu->menu.styles = safe_malloc(sizeof(uint8_t) * tmplt->len);
+    st_menu->menu.template = tmplt;
+    st_menu->menu.styles = (uint8_t *)safe_malloc(sizeof(uint8_t) * tmplt->len);
 
-    bt_menu->menu.styles[0] = s_style;
+    // Menu starts off as unfocused.
+    uint8_t i;
+    for (i = 0; i < tmplt->len; i++) {
+        st_menu->menu.styles[i] = uf_style;
+    }
+
+    // Store selection styles.
+    st_menu->selection_style = s_style;
+    st_menu->deselection_style = ds_style;
+    st_menu->unfocused_style = uf_style;
+
+    // Default all selections to 0.
+    st_menu->buffer_selection = 0;
+    st_menu->screen_selection = 0;
+    st_menu->selection = 0;
+
+    st_menu->redraw = FULL_REDRAW;
+
+    return st_menu;
+}
+
+void focus_simple_text_menu(simple_text_menu *st_menu) {
+    // TODO...
+}
+
+void unfocus_simple_text_menu(simple_text_menu *st_menu) {
+    uint8_t len = st_menu->menu.template->len;
 
     uint8_t i;
-    for (i = 1; i < tmplt->len; i++) {
-        bt_menu->menu.styles[i] = ds_style;
+    for (i = 0; i < len; i++) {
+        st_menu->menu.styles[i] = st_menu->unfocused_style;
     }
 
-    bt_menu->selection_style = s_style;
-    bt_menu->deselection_style = ds_style;
-
-    bt_menu->selection = 0;
-
-    return bt_menu;
+    st_menu->redraw = FULL_REDRAW;
 }
 
-uint8_t update_basic_text_menu(basic_text_menu *bt_menu) {
-    uint8_t format = bt_menu->menu.template->format;
+void update_simple_text_menu(simple_text_menu *st_menu) {
+    uint8_t format = st_menu->menu.template->format;
 
     if (
         ((format == MENU_VERTICAL && (key_press(c_Up) || key_press(c_8))) ||
         (format == MENU_HORIZONTAL && (key_press(c_Left) || key_press(c_4)))) &&
-        bt_menu->selection != 0 
+        st_menu->selection != 0 
     ) {
-        bt_menu->menu.styles[bt_menu->selection] = bt_menu->deselection_style;
-        bt_menu->menu.styles[--(bt_menu->selection)] = bt_menu->selection_style;
-
-        return 1;
+        st_menu->menu.styles[st_menu->selection] = st_menu->deselection_style;
+        st_menu->menu.styles[--(st_menu->selection)] = st_menu->selection_style;
     } else if (
         ((format == MENU_VERTICAL && (key_press(c_Down) || key_press(c_5))) ||
         (format == MENU_HORIZONTAL && (key_press(c_Right) || key_press(c_6)))) &&
-        bt_menu->selection != bt_menu->menu.template->len - 1
+        st_menu->selection != st_menu->menu.template->len - 1
     ) {
-        bt_menu->menu.styles[bt_menu->selection] = bt_menu->deselection_style;
-        bt_menu->menu.styles[++(bt_menu->selection)] = bt_menu->selection_style;
+        st_menu->menu.styles[st_menu->selection] = st_menu->deselection_style;
+        st_menu->menu.styles[++(st_menu->selection)] = st_menu->selection_style;
+    }
 
-        return 1;
+    // Specify to renderer if redraw must occur.
+    st_menu->redraw = st_menu->selection == st_menu->buffer_selection 
+        ? NO_REDRAW 
+        : PARTIAL_REDRAW;
+}
+
+uint8_t render_simple_text_menu_nc(simple_text_menu *st_menu) {
+    if (st_menu->redraw == FULL_REDRAW) {
+        // Full menu redraw required.
+        render_text_menu_nc(&(st_menu->menu));
+
+        goto ROTATE_SELECTIONS;
+    }
+
+    if (st_menu->redraw == PARTIAL_REDRAW) {
+        // This signifies the selection has changed!
+
+        // Now we are drawing over the buffer.
+        render_text_menu_button_nc(&(st_menu->menu), st_menu->buffer_selection);
+        render_text_menu_button_nc(&(st_menu->menu), st_menu->selection);
+
+        goto ROTATE_SELECTIONS;
     }
 
     return 0;
+
+ROTATE_SELECTIONS:
+    st_menu->buffer_selection = st_menu->screen_selection;
+    st_menu->screen_selection = st_menu->selection;
+
+    return 1;
 }
 
-void del_basic_text_menu(basic_text_menu *bt_menu) {
-    free(bt_menu->menu.styles);
-    free(bt_menu);
+void del_simple_text_menu(simple_text_menu *st_menu) {
+    free(st_menu->menu.styles);
+    free(st_menu);
 }
 
--- cutil/menu.h
+++ cutil/menu.h
@@ -26,10 +26,22 @@ typedef struct {
     uint8_t label_width_scale;
     uint8_t label_height_scale;
 
+    // Coordinates of the menu.
+    uint16_t x;
+    uint8_t y;
+
     // Number of buttons.
     // (i.e. number of entries in the labels and styles arrays)
     uint8_t len;
 } text_menu_template;
+
+
+// Way to deal with buffering issues....
+typedef struct {
+    uint8_t style;
+    uint8_t screen_style;
+    uint8_t buffer_style;
+} buffered_styling;
 
 typedef struct {
     const text_menu_template *template;
@@ -38,10 +50,18 @@ typedef struct {
 
 // Render a specific button of a menu.
 // This is meant to be used for partial redraw.
-void render_text_menu_button_nc(text_menu *menu, uint16_t x, uint8_t y, uint8_t i);
+void render_text_menu_button_nc(text_menu *menu, uint8_t i);
 
 // Render a text menu.
-void render_text_menu_nc(text_menu *menu, uint16_t x, uint8_t y);
+void render_text_menu_nc(text_menu *menu);
+
+// Specifications for menu rendering types.
+#define NO_REDRAW       0
+#define FULL_REDRAW     1
+#define PARTIAL_REDRAW  2
+
+// Basic Menu needs to be refactored...
+// Focusing, and drawing mechanics should be used here...
 
 typedef struct {
     text_menu menu;
@@ -49,17 +69,42 @@ typedef struct {
     uint8_t selection_style;
     uint8_t deselection_style;
 
+    // The selected button as seen in the buffer.
+    // Used for partial redraw.
+    uint8_t buffer_selection; 
+
+    // The selected button as seen on the screen.
+    uint8_t screen_selection;
+
+    // The actual selection.
     uint8_t selection;
-} basic_text_menu;
 
-// Create a new basic text menu.
-basic_text_menu *new_basic_text_menu(const text_menu_template *tmplt, uint8_t s_style, uint8_t ds_style);
+    uint8_t unfocused_style;
 
-// Read keypad input to update the menu.
-// Returns 1 if a redraw is needed.
-uint8_t update_basic_text_menu(basic_text_menu *bt_menu);
+    // Specify if and how the menu should be redrawn.
+    uint8_t redraw;
+} simple_text_menu;
 
-// Delete a basic text menu.
-void del_basic_text_menu(basic_text_menu *bt_menu);
+// Create a new simple text menu. (Starts unfocused always)
+simple_text_menu *new_simple_text_menu(const text_menu_template *tmplt, 
+    uint8_t s_style, uint8_t ds_style, uint8_t uf_style);
+
+// Focus a simple text menu.
+void focus_simple_text_menu(simple_text_menu *st_menu);
+
+// Unfocus a simple text menu.
+void unfocus_simple_text_menu(simple_text_menu *st_menu);
+
+// Checks keypad input for updating selection.
+// NOTE, this should only be called if the menu is focused.
+// Otherwise, the rendering may not draw as expected.
+void update_simple_text_menu(simple_text_menu *st_menu);
+
+// Render the simple text menu according to its redraw specs.
+// If drawing actually occurs 1 will be returned, otherwise 0.
+uint8_t render_simple_text_menu_nc(simple_text_menu *st_menu);
+
+// Delete a simple text menu.
+void del_simple_text_menu(simple_text_menu *st_menu);
 
 #endif
