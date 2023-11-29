a4565d3476194fadb0a0d724f22b372fb94b759b
chathamabate <chathamabate@gmail.com>
2023-11-29 18:33:38 -0500

    Grid View

--- ti-farmer/src/main.cpp
+++ ti-farmer/src/main.cpp
@@ -23,12 +23,40 @@ using namespace cxxutil;
 
 // Looking good so far!
 static void viewBitGrid(cxxutil::data::BitGrid *bg) {
-    // TODO: write grid drawing algo.
+    const uint8_t SIDE_LEN = 8; // 5px per side.
+    
+    const uint8_t PATH_COLOR = 22; 
+    const uint8_t BARR_COLOR = 19;
+                                
+    size_t rowBound = bg->getRows();
+    if (rowBound * SIDE_LEN > GFX_LCD_HEIGHT) {
+        rowBound = GFX_LCD_HEIGHT / SIDE_LEN;
+    }
+                                
+    size_t colBound = bg->getCols();
+    if (colBound * SIDE_LEN > GFX_LCD_WIDTH) {
+        colBound = GFX_LCD_WIDTH / SIDE_LEN;
+    }
+
+    gfx_SetColor(PATH_COLOR);
+    gfx_FillRectangle(0, 0, 
+            colBound * SIDE_LEN, rowBound * SIDE_LEN);
+
+    gfx_SetColor(BARR_COLOR);
+
+    for (size_t r = 0; r < rowBound; r++) {
+        for (size_t c = 0; c < colBound; c++) {
+            if (!(bg->get(r, c))) {
+                gfx_FillRectangle(c * SIDE_LEN, r * SIDE_LEN, 
+                        SIDE_LEN, SIDE_LEN);
+            }
+        }
+    } 
 }
 
 void innerMain(void) {
     cxxutil::data::BitGrid *bg = 
-        tif::model::createMaze(1, 10, 10);
+        tif::model::createMaze(1, 15, 15);
 
     gfx_Begin();
     core::MemoryTracker::ONLY->setMER(core::GraphicsMemoryExitRoutine::ONLY);
