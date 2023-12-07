2e1ecb07cbe0fb0d2b065dd7b9c822a44545c22e
chathamabate <chathamabate@gmail.com>
2023-12-06 21:25:33 -0500

    API update

--- ti-farmer/src/model/state.h
+++ ti-farmer/src/model/state.h
@@ -35,17 +35,18 @@ namespace tif { namespace model {
 
         // Todays date.
         statics::day_count_t date;
+
+        // NOTE: we are going to be using a 1D array approach for
+        // all dynamic arrays in this state.
         
-        // Total number of crops harvested on this planet.
         cxxutil::core::SafeArray<cxxutil::core::U24> *cropCounts;
+
+        cxxutil::core::SafeArray<bool> * *goals;
 
         // Feature inventory on this planet.
         cxxutil::core::SafeArray<FeatureCount> *featureCounts;
 
-        // Whether or not individual goal rewards have been collected yet.
-        cxxutil::core::SafeArray<cxxutil::core::SafeArray<bool> *> *goals;
-
-        cxxutil::core::SafeArray<cxxutil::core::SafeArray<CellState> *> *grid; 
+        cxxutil::core::SafeArray<CellState> *grid; 
         
     public:
         PlanetState(const statics::planet_t *p); 
@@ -65,33 +66,49 @@ namespace tif { namespace model {
             return this->date;
         }
 
+        // NOTE: The below calls will assume all indeces are valid
+        // given grid dimmensions and season.
+        //
+        // NOTE: Bank balance is a universal state and thus out of
+        // planet state. No method below knows anything about "tokens"
+        // It is the responsibility of the universal state to make sure
+        // "tokens" are handled correctly.
+
         // Go to tomorrow. (This will account for season changes)
         void incDate();
 
         // Whether or not the given cell is an empty plot.
-        bool isPlantable(uint8_t r, uint8_t c);
+        bool isPlantable(uint8_t r, uint8_t c) const;
 
         // You can only ever plant crops in-season on a plot.
-        bool plantCrop(uint8_t r, uint8_t c, uint8_t cropInd);
+        bool plant(uint8_t r, uint8_t c, uint8_t cropInd);
 
         // If the given cell is a plot with a ripe crop.
-        bool isHarvestable(uint8_t r, uint8_t c);
+        bool isHarvestable(uint8_t r, uint8_t c) const;
 
         // Harvest crops from the given cell.
         bool harvest(uint8_t r, uint8_t c);
 
         // Whether or not the given cell holds a feature.
-        bool isClearable(uint8_t r, uint8_t c);
+        bool isClearable(uint8_t r, uint8_t c) const;
 
         // Clear a cells feature.
         bool clear(uint8_t r, uint8_t c);
 
         // True when we have excess of a feature in our inventory,
         // and the given cell is clear.
-        bool isPlaceable(uint8_t r, uint8_t c, uint8_t featureInd);
+        bool isPlaceable(uint8_t r, uint8_t c, uint8_t featureInd) const;
 
         // Place a feature.
         bool place(uint8_t r, uint8_t c, uint8_t featureInd);
         
+        // Whether or not a specific goal has been collected.
+        bool isCollected(uint8_t seasonInd, uint8_t cropInd, uint8_t goalInd) const;
+
+        // Whether or not the requirements of a goal have been passed.
+        bool isPassed(uint8_t seasonInd, uint8_t cropInd, uint8_t goalInd) const;
+
+        // Collect a goal.
+        bool collect(uint8_t seasonInd, uint8_t cropInd, uint8_t goalInd);
     };
 }}
