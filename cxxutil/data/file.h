48cce48d474ae4e91b1cc87cf96ef7e66dd3d57b
chathamabate <chathamabate@gmail.com>
2023-12-10 16:27:51 -0500

    Deep/Shallow distinction

--- cxxutil/data/file.h
+++ cxxutil/data/file.h
@@ -36,24 +36,96 @@ namespace cxxutil { namespace data {
         virtual bool read(uint8_t handle, T *dest) = 0;
     };
 
-    // Here are some standard ways for reading and writing 
-    // arrays!
+    // NOTE:
+    // Below are a few helpers for reading and writing arrays to/from files.
+    // The "Shallow" version is for arrays which are self contained.
+    // i.e. an array of integers or structs. These can just be copied.
+    //
+    // The "Deep" Version allows for recursion while reading from a file.
+    // Each element can be created using its own reader/writer.
 
-    // Safe Array Helpers.
+    template <typename T>
+    class ShallowArrayFileWriter : public FileWriter<core::SafeArray<T> *> {
+    public:
+        ShallowArrayFileWriter() : ShallowArrayFileWriter(core::CXX_DEF_CHNL) {
+        }
+
+        ShallowArrayFileWriter(uint8_t chnl) : FileWriter<core::SafeArray<T> *>(chnl) {
+        }
+
+        virtual ~ShallowArrayFileWriter() {
+        }
+
+        virtual bool write(uint8_t handle, core::SafeArray<T> *element) override {
+            const size_t len = element->getLen();
+
+            size_t lenRes = ti_Write(&len, sizeof(size_t), 1, handle);
+
+            if (lenRes != 1) {
+                return false;
+            }
+
+            const T *arr = element->getArr();
+            size_t arrRes = ti_Write(arr, sizeof(T), len, handle);
+
+            if (arrRes != len) {
+                return false;
+            }
+
+            return true;
+        }
+    };
+
+    template <typename T>
+    class ShallowArrayFileReader : public FileReader<core::SafeArray<T> *> {
+    public:
+        ShallowArrayFileReader() 
+            : ShallowArrayFileReader(core::CXX_DEF_CHNL) {
+        }
+
+        ShallowArrayFileReader(uint8_t chnl) 
+            : FileReader<core::SafeArray<T> *>(chnl) {
+        }
+
+        virtual ~ShallowArrayFileReader() {
+        }
+
+        virtual bool read(uint8_t handle, core::SafeArray<T> **dest) override {
+            size_t len;
+            size_t lenRes = ti_Read(&len, sizeof(size_t), 1, handle);    
+
+            if (lenRes != 1) {
+                return false;
+            }
+
+            core::SafeArray<T> *arr = new core::SafeArray<T>(this->getChnl(), len);
+            size_t arrRes = ti_Read(arr->getArrMut(), sizeof(T), len, handle);
+
+            if (arrRes != len) {
+                delete arr;
+                return false;
+            }
+
+            *dest = arr;
+
+            return true;
+        }
+    };
+
     template <typename T> 
-    class ArrayFileWriter : public FileWriter<core::SafeArray<T> *> {
+    class DeepArrayFileWriter : public FileWriter<core::SafeArray<T> *> {
     private:
         FileWriter<T> * const elementWriter;
     public:
-        ArrayFileWriter(FileWriter<T> *ew) 
-            : ArrayFileWriter(core::CXX_DEF_CHNL, ew) {
+        DeepArrayFileWriter(FileWriter<T> *ew) 
+            : DeepArrayFileWriter(core::CXX_DEF_CHNL, ew) {
         }
 
-        ArrayFileWriter(uint8_t chnl, FileWriter<T> *ew) 
+        DeepArrayFileWriter(uint8_t chnl, FileWriter<T> *ew) 
             : FileWriter<core::SafeArray<T> *>(chnl), elementWriter(ew) {
         }
 
-        virtual ~ArrayFileWriter() {
+        virtual ~DeepArrayFileWriter() {
         }
 
         virtual bool write(uint8_t handle, core::SafeArray<T> *element) override {
@@ -81,20 +153,20 @@ namespace cxxutil { namespace data {
     };
 
     template <typename T>
-    class ArrayFileReader : public FileReader<core::SafeArray<T> *> {
+    class DeepArrayFileReader : public FileReader<core::SafeArray<T> *> {
     private:
         FileReader<T> * const elementReader;
 
     public:
-        ArrayFileReader(FileReader<T> *er) 
-            : ArrayFileReader(core::CXX_DEF_CHNL, er) {
+        DeepArrayFileReader(FileReader<T> *er) 
+            : DeepArrayFileReader(core::CXX_DEF_CHNL, er) {
         }
 
-        ArrayFileReader(uint8_t chnl, FileReader<T> *er) 
+        DeepArrayFileReader(uint8_t chnl, FileReader<T> *er) 
             : FileReader<core::SafeArray<T> *>(chnl), elementReader(er) {
         }
 
-        virtual ~ArrayFileReader() {
+        virtual ~DeepArrayFileReader() {
         }
 
         virtual bool read(uint8_t handle, core::SafeArray<T> **dest) override {
