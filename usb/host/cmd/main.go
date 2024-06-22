cec105ca1caa66effde05063693527ce18328c03
chathamabate <chathamabate@gmail.com>
2024-06-22 11:55:26 -0600

    Progress on Go Host

--- usb/host/cmd/main.go
+++ usb/host/cmd/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+
+import (
+	"fmt"
+	"log"
+	"github.com/google/gousb"
+)
+
+// TI-84 Vendor ID.
+const VENDOR_ID = 0x0451
+const PRODUCT_ID = 0xE008
 
 func main() {
-    fmt.Println("Hello, World!")
+	// Initialize a new Context.
+	ctx := gousb.NewContext()
+	defer ctx.Close()
+
+    // Maybe first let's just read all devices??
+    dev, err := ctx.OpenDeviceWithVIDPID(VENDOR_ID, PRODUCT_ID)
+	if err != nil || dev == nil {
+		log.Fatalf("Could not open a device: %v", err)
+	}
+	defer dev.Close()
+
+    fmt.Println("Device Successfully Found") 
+    productName, err := dev.Product()
+    if err != nil {
+        log.Fatalf("Could not get product name")
+    }
+    fmt.Printf("Product Name: %s\n", productName)
 }
+
--- usb/host/go.mod
+++ usb/host/go.mod
@@ -1,3 +1,5 @@
 module github.com/chathamabate/ti-code/usb/host
 
 go 1.21.5
+
+require github.com/google/gousb v1.1.3 // indirect
