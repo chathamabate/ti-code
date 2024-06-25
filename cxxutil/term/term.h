c0689e167110dee48b6610047b02220d6195606d
Chatham Abate <chathamabate@gmail.com>
2024-06-24 18:51:26 -0600

    Colors Start

--- cxxutil/term/term.h
+++ cxxutil/term/term.h
@@ -4,6 +4,37 @@
 #include <cxxutil/core/mem.h>
 
 namespace cxxutil { namespace term {
+    // Supported font effects:
+    // Foreground Color (8 Normal colors + 8 Bright Colors) = 16 total
+    // F
+    //
+    //
+
+    // Ansi color offsets.
+    constexpr uint8_t ANSII_BLACK = 0;
+    constexpr uint8_t ANSII_RED = 1;
+    constexpr uint8_t ANSII_GREEN = 2;
+    constexpr uint8_t ANSII_YELLOW = 3;
+    constexpr uint8_t ANSII_BLUE = 4;
+    constexpr uint8_t ANSII_MAGENTA = 5;
+    constexpr uint8_t ANSII_CYAN = 6;
+    constexpr uint8_t ANSII_WHITE = 7;
+    
+    struct cell_style_t {
+        uint24_t fgColor : 3; 
+        uint24_t bgColor : 3;
+        uint24_t strong : 1;  
+        uint24_t underline : 1;
+        uint24_t slowBlink : 1;
+        uint24_t fastBlink : 1;
+        uint24_t strikeThru : 1;
+    };
+
+    struct cell_state_t {
+        cell_style_t style;
+        char character;
+    };
+    
     // The point of this will ultimately be to allow for control character
     // sequences to actually be interpreted by the terminal...
     // This will be pretty bare bones tbh, but still cool!
@@ -16,6 +47,5 @@ namespace cxxutil { namespace term {
         // Maybe store an entire backlog of text???
         // hmmmm... scrolling could be cool...
         
-        void outc() // ?
     };
 }}
